f6767f0cfc7331ac99cecc56fbcf144654a34244
zyx-0314-avalanche <ramirezian037@gmail.com>
2025-10-17 13:46:24 +0800

    feat: add signup functionality with validation and view for user registration

--- web-service/app/Config/Routes.php
+++ web-service/app/Config/Routes.php
@@ -6,12 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-// User exposed pages
+// Public pages
 $routes->get('/', 'Users::showLandingPage');
 $routes->get('/mood-board', 'Users::showMoodBoard');
 $routes->get('/road-map', 'Users::showRoadMap');
 
-// Auth
+// Auth pages
 $routes->get('/login', 'Auth::showLoginPage');
+$routes->get('/signup', 'Auth::showSignupPage');
+
+// Auth actions
 $routes->post('/login', 'Auth::login');
 $routes->post('/logout', 'Auth::logout');
+$routes->post('/signup', 'Auth::signup');
