8979fadecd40eda0bcd62b535b3886b4179c1dba
zyx-0314-avalanche <ramirezian037@gmail.com>
2025-10-18 18:03:24 +0800

    feat: Create migration for services table with necessary fields

---
+++ web-service/app/Database/Migrations/2025-09-11-000715_CreateServicesTable.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateServicesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'cost' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'default' => 0,
+            ],
+            'is_available' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 1,
+            ],
+            'is_active' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 1,
+            ],
+            'inclusions' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'banner_image' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('services', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('services', true);
+    }
+}
